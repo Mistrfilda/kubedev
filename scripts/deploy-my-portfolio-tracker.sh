19e20dba223955d3e5b7ea935fd5ff86ace2668f
Filip Kuchar <filda.kuchar@seznam.cz>
2024-10-20 14:21:53 +0200

    update deploy script

--- scripts/deploy-my-portfolio-tracker.sh
+++ scripts/deploy-my-portfolio-tracker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-microk8s kubectl apply -f ../cronjobs/my-portfolio-tracker-cronjobs.yaml
-microk8s kubectl apply -f ../deployment/my-portfolio-tracker-nginx-deployement.yml
-microk8s kubectl apply -f ../deployment/my-portfolio-tracker-php-deployement.yml
+microk8s kubectl apply -f /var/kubedev/kubedev/cronjobs/my-portfolio-tracker-cronjobs.yaml
+microk8s kubectl apply -f /var/kubedev/kubedev/deployment/my-portfolio-tracker-nginx-deployement.yml
+microk8s kubectl apply -f /var/kubedev/kubedev/deployment/my-portfolio-tracker-php-deployement.yml
