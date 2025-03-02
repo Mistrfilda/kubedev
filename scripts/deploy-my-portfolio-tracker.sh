b95d6e9137d87ffbd3e27ebc82d87baf27bce366
Filip Kuchar <filda.kuchar@seznam.cz>
2025-03-02 12:22:08 +0100

    run database migrations + declare rabbitmq queues on deploy

--- scripts/deploy-my-portfolio-tracker.sh
+++ scripts/deploy-my-portfolio-tracker.sh
@@ -4,3 +4,7 @@ microk8s kubectl apply -f /var/kubedev/kubedev/cronjobs/my-portfolio-tracker-cro
 microk8s kubectl apply -f /var/kubedev/kubedev/cronjobs/my-portfolio-tracker-queues-consumers.yaml
 microk8s kubectl apply -f /var/kubedev/kubedev/deployment/my-portfolio-tracker-nginx-deployement.yaml
 microk8s kubectl apply -f /var/kubedev/kubedev/deployment/my-portfolio-tracker-php-deployement.yaml
+
+# Po nasazení okamžité spuštění konkrétních CronJobů
+microk8s kubectl create job --from=cronjob/php-database-migrations manual-cronjob-run-php-database-migration
+microk8s kubectl create job --from=cronjob/php-rabbitmq-queues manual-cronjob-run-php-rabbitmq-queues-2
